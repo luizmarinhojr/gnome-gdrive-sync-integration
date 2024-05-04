2e77d4d73f4e657c81810e6d3f5a7bcd09b72a27
Luiz Carlos <luizcmoficial@gmail.com>
2024-05-03 23:50:32 -0300

    performance

--- app/background.py
+++ app/background.py
@@ -1,21 +1,20 @@
 from pystray import Icon
 from PIL import Image
 from threading import Thread
+import os
 
 
 class BackgroundIcon:
     def __init__(self, e = ''):
         self.main()
-    
 
     def stopIcon(self):
         print('Stop Icon')
         self.icon.stop()
 
-
     def main(self):
         print('Starting Icon Background')
-        image = Image.open("./assets/icon.png")  # Substitua "icone.png" pelo caminho para o ícone desejado
+        image = Image.open(os.path.join(os.path.expanduser('~'), '.local', 'share', 'GDriveSync', 'linux', 'assets', 'icon.png'))  # Substitua "icone.png" pelo caminho para o ícone desejado
         self.icon = Icon("GSyncDrive", image)
         self.running = Thread(target=self.icon.run)
         self.running.start()
